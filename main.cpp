f41e76825727e798fe7815dc809855a27a3d21e2
KaaKangSatir <141976349+KaaKangSatir@users.noreply.github.com>
2023-10-27 08:19:37 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ extern "C" void OnModLoad()
     pGTASA = aml->GetLib("libGTASA.so");
 
     aml->Unprot(pGTASA + 0x952CB0, 8);
-    SET_TO(m_f3rdPersonCHairMultX, pGTASA + 0x952CB0);
+    ATUR_POSISI(m_f3rdPersonCHairMultX, pGTASA + 0x952CB0);
     SET_TOO(m_f3rdPersonCHairMultY, pGTASA + 0x952CB4);
     HOOKPLT(DrawCrosshair, pGTASA + 0x672880);
 }
